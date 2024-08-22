57c5ee84fe0b7e98b8aa46e4536852a9edd47558
BISCVITE30 <gonnki2004@gmail.com>
2024-08-22 21:17:28 +0200

    !

---
+++ lesson2-redux-intro/task2/src/store.js
@@ -0,0 +1,49 @@
+import { createStore } from 'redux';
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+const RESET = 'COUNTER/RESET';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+    payload: '+1',
+  };
+};
+
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+    payload: '-1'
+  };
+};
+export const reset = () => {
+  return {
+    type: RESET,
+  };
+};
+
+const initialState = {
+  history: [],
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+    case DECREMENT:
+      return {
+        ...state,
+        history: [...state.history, action.payload],
+      };
+    case RESET:
+      return {
+        ...state,
+        history: [],
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
