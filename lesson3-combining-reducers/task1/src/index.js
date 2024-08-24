afed1399c4937dd0c4187690100e9f61e10f55e3
BISCVITE30 <gonnki2004@gmail.com>
2024-08-24 18:04:22 +0200

    !

--- lesson3-combining-reducers/task1/src/index.js
+++ lesson3-combining-reducers/task1/src/index.js
@@ -1,8 +1,11 @@
+import { decrement, increment } from './counter.actions';
 import store from './store';
 import { addUser, deleteUser } from './users.actions';
 
 store.dispatch(addUser({ id: 76, name: 'Sarah' }));
 store.dispatch(addUser({ id: 77, name: 'John' }));
+store.dispatch(decrement());
+store.dispatch(increment());
 
 store.subscribe(() => {
   console.log('Изменение состояния:', store.getState());
