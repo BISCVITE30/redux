bb36bd3dd494cbacfdad6b35d58760786635b550
BISCVITE30 <gonnki2004@gmail.com>
2024-08-24 13:58:56 +0200

    !

--- lesson2-redux-intro/task3/src/store.js
+++ lesson2-redux-intro/task3/src/store.js
@@ -1,14 +1,7 @@
 import { createStore } from 'redux';
 import { userReducer } from './users.reducer';
-import { addUser, deleteUser } from './users.actions';
 
 export const store = createStore(userReducer);
 
 export default store;
 
-store.dispatch(addUser('Sarah', 10));
-store.dispatch(addUser('Sarah', 30));
-store.dispatch(addUser('Sarah', 20));
-store.dispatch(deleteUser(20));
-
-console.log(store.getState());
