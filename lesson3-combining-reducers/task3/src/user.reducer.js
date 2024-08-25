caec1a48d9643a3436f2d6ab7baa8ef2cab8e952
BISCVITE30 <gonnki2004@gmail.com>
2024-08-25 21:48:49 +0200

    !

--- lesson3-combining-reducers/task3/src/user.reducer.js
+++ lesson3-combining-reducers/task3/src/user.reducer.js
@@ -5,7 +5,13 @@ const initialState = null;
 export const userReducer = (state = initialState, action) => {
   switch (action.type) {
     case USER_SET:
-      return state ? [...state, action.payload] : [action.payload];
+      if (!state) {
+        return [action.payload];
+      }
+
+      return state.map(user =>
+        user.id === action.payload.id ? { ...user, ...action.payload } : user,
+      );
 
     case USER_DELETE:
       return state ? state.filter(user => user.id !== action.payload) : null;
